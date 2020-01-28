ede0ea0a6339b7ad7c5e6823bbffdac1c824c324
Parth <kparth2010@gmail.com>
2020-01-29 02:56:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express =require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname +"/index.html");
+})
+
+app.get("/about", function(req,res){
+    res.sendFile(__dirname +"/index2.html");
+})
+
+app.listen(3000, function(){
+    console.log("server started");
+});
